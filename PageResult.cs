2e8f0e8d95417fd351fdbd1176269fedd143768e
ramazankurt56 <93058427+ramazankurt56@users.noreply.github.com>
2024-03-28 14:58:29 +0300

    Add project files.

---
+++ PageResult.cs
@@ -0,0 +1,25 @@
+﻿namespace Lunavex.EFCore.Pagination;
+public class PageResult<T>
+{
+    public IList<T> Datas { get; set; }
+
+    public int PageNumber { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int TotalPages { get; set; }
+
+    public bool IsFirstPage { get; set; }
+
+    public bool IsLastPage { get; set; }
+
+    public PageResult(IList<T> datas, int pageNumber, int pageSize, int totalCount)
+    {
+        Datas = datas;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalPages = ((totalCount + pageSize > 0) ? ((int)Math.Ceiling((double)totalCount / (double)pageSize)) : 0);
+        IsFirstPage = PageNumber == 1;
+        IsLastPage = PageNumber == TotalPages;
+    }
+}
---
+++ PagesListQuerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lunavex.EFCore.Pagination;
+public static class PagesListQuerableExtensions
+{
+    public static async Task<PageResult<T>> ToPagedListAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize, CancellationToken cancellationToken = default) where T : class
+    {
+        int count = await source.CountAsync(cancellationToken);
+        if (count > 0)
+        {
+            return new PageResult<T>(await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).AsNoTracking()
+                .ToListAsync(cancellationToken), pageNumber, pageSize, count);
+        }
+
+        return new PageResult<T>(null, 0, 0, 0);
+    }
+}
